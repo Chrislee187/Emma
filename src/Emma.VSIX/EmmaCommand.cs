8e5c4cd8ceed8365d68dde45445272c13f19d9a5
Chris Lee <cle@planday.com>
2021-01-26 14:10:56 +0000

    Fix bug with control init

--- src/Emma.VSIX/EmmaCommand.cs
+++ src/Emma.VSIX/EmmaCommand.cs
@@ -10,31 +10,15 @@ namespace Emma.VSIX
 {
     internal sealed class EmmaCommand
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
         public const int CommandId = 257;
 
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
-        // public static readonly Guid CommandSet = new Guid("9cc1062b-4c82-46d2-adcb-f5c17d55fb85");
-        public static readonly Guid CommandSet = new Guid("CCC2699A-6608-42EA-A6A6-3764E617D764");
+        public static readonly Guid CommandSet = new Guid("ccc2699a-6608-42ea-a6a6-3764e617d764");
 
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
-        private readonly AsyncPackage package;
+        private readonly AsyncPackage _package;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EmmaCommand"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
         private EmmaCommand(AsyncPackage package, OleMenuCommandService commandService)
         {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            _package = package ?? throw new ArgumentNullException(nameof(package));
             commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
 
             var menuCommandID = new CommandID(CommandSet, CommandId);
@@ -42,55 +26,33 @@ namespace Emma.VSIX
             commandService.AddCommand(menuItem);
         }
 
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
         public static EmmaCommand Instance
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
-        {
-            get
-            {
-                return this.package;
-            }
-        }
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider => 
+            _package;
 
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Switch to the main thread - the call to AddCommand in EmmaCommand's constructor requires
-            // the UI thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            var commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
 
             // NOTE: hotkey is regsitered in VSCommandsTable.csvt
             // see also https://stackoverflow.com/questions/10392622/how-to-make-a-shortcut-to-run-a-vsix-method
             Instance = new EmmaCommand(package, commandService);
         }
 
-        /// <summary>
-        /// Shows the tool window when the menu item is clicked.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
         private void Execute(object sender, EventArgs e)
         {
-            this.package.JoinableTaskFactory.RunAsync(async delegate
+            this._package.JoinableTaskFactory.RunAsync(async delegate
             {
-                ToolWindowPane window = await this.package.ShowToolWindowAsync(
-                    typeof(EmmaMainToolWindowPane), 0, true, this.package.DisposalToken);
-                if ((null == window) || (null == window.Frame))
+                var window = await this._package.ShowToolWindowAsync(
+                    typeof(EmmaMainToolWindowPane), 0, true, this._package.DisposalToken);
+                if (window?.Frame == null)
                 {
                     throw new NotSupportedException("Cannot create tool window");
                 }
--- src/Emma.XamlControls/MainEmmaToolWindowControl.xaml.cs
+++ src/Emma.XamlControls/MainEmmaToolWindowControl.xaml.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Emma.XamlControls
+{
+    /// <summary>
+    /// Interaction logic for MainEmmaToolWindowControl.xaml
+    /// </summary>
+    public partial class MainEmmaToolWindowControl : UserControl
+    {
+        public MainEmmaToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SearchButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "Emma");
+        }
+
+    }
+}
